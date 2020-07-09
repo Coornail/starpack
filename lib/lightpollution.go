0008513f1f6269415388363f19822a312d9c1587
Kornel Lugosi <coornail@gmail.com>
2020-07-09 10:19:49 +0200

    Only downscale 3x3 for lightmask estimation

--- lib/lightpollution.go
+++ lib/lightpollution.go
@@ -6,14 +6,14 @@ import (
 	"github.com/disintegration/imaging"
 )
 
-const downSamplePoints = 16
+const downSamplePoints = 3
 
 // EstimateLightPollutionMask generates a mask to remove it from the image.
 // Based on the idea from https://benedikt-bitterli.me/astro/ .
 func EstimateLightPollutionMask(img image.Image) image.Image {
-	downsampled := imaging.Resize(img, downSamplePoints, downSamplePoints, imaging.Gaussian)
+	downsampled := imaging.Resize(img, downSamplePoints, downSamplePoints, imaging.Lanczos)
 	// @todo improve on upscaling.
-	upsampled := imaging.Resize(downsampled, img.Bounds().Max.X, img.Bounds().Max.Y, imaging.Lanczos)
+	upsampled := imaging.Resize(downsampled, img.Bounds().Max.X, img.Bounds().Max.Y, imaging.MitchellNetravali)
 	upsampled = imaging.Blur(upsampled, 1.5)
 
 	return upsampled
